b2a83ef366b267649993cf0b87b94c70e6f46e5e
Bart Biemans <bart.biemans@student.kdg.be>
2018-03-13 19:16:28 +0100

    extra aanpassingen jsonreader

--- DemoPolitiekeBarometer/DemoPolitiekeBarometer/JsonReader.cs
+++ DemoPolitiekeBarometer/DemoPolitiekeBarometer/JsonReader.cs
@@ -12,44 +12,80 @@ namespace DemoPolitiekeBarometer
     {
 
         private List<Tweet> tweets = new List<Tweet>();
+
+        private List<string> words = new List<string>();
+        private List<string> hashtags = new List<string>();
+        private bool inArray = false;
+        private string tokentypevalue = null;
+        private StringBuilder politican = new StringBuilder();
+
         public void readJson()
         {
             JsonTextReader reader = new JsonTextReader(new StringReader(File.ReadAllText("../../Resources/TextDump.json")));
             while (reader.Read())
             {
-                /*Console.WriteLine(reader.TokenType + " TokenType");
-                if (reader.TokenType.ToString().Equals("StartArray")){
-                    while (reader.TokenType.Equals("String"))
-                    {
-                        Console.WriteLine("In while loop: " + reader.Value);
-                    }
-                }*/
-                if (reader.Value != null)
-                {
-                    if (reader.Value.ToString().Equals("records"))
-                    {
-                        Console.WriteLine("Start alle tweets");
-                    }
-                    else
-                    {
-                        Console.WriteLine(reader.Value);
-                    }
+                if (reader.TokenType.ToString().Equals("StartObject")) {
+                    Console.WriteLine("Start Tweet");
                 }
-                else
-                {
-                    if (reader.TokenType.ToString().Equals("StartObject"))
-                    {
-                        Console.WriteLine("Begin Tweet");
-                    }
-                    
+                if (reader.TokenType.ToString().Equals("PropertyName") && reader.Value.ToString().Equals("words")) {
+                    inArray = true;
+                    tokentypevalue = "words";
                 }
-                Console.ReadLine();
+                if (reader.TokenType.ToString().Equals("PropertyName") && reader.Value.ToString().Equals("politician"))
+                {
+                    inArray = true;
+                    tokentypevalue = "politician";
+                }
+                if (reader.TokenType.ToString().Equals("PropertyName") && reader.Value.ToString().Equals("hashtags"))
+                {
+                    inArray = true;
+                    tokentypevalue = "hashtags";
+                }
+                if (reader.TokenType.ToString().Equals("StartArray")) {
+                        Console.WriteLine("Start Array");
+                }
+                if (reader.TokenType.ToString().Equals("EndArray")){
+                    inArray = false;
+
+                }
 
 
-            }
-
-
+                if (reader.TokenType.ToString().Equals("String") && inArray && tokentypevalue.Equals("words")) {
+                    words.Add(reader.Value.ToString());
+                }
+                if (reader.TokenType.ToString().Equals("String") && inArray && tokentypevalue.Equals("politician"))
+                {
+                    politican.Append(reader.Value.ToString());
+                    politican.Append(" ");
+                }
+                if (reader.TokenType.ToString().Equals("String") && inArray && tokentypevalue.Equals("hashtags"))
+                {
+                    hashtags.Add(reader.Value.ToString());
+                }
+                if (reader.TokenType.ToString().Equals("EndObject"))
+                {
+                    Console.WriteLine("End Tweet");
+                    Console.ReadLine();
+                    Console.WriteLine("WORDS: ");
+                    for (int i = 0; i < words.Count; i++)
+                    {
+                        Console.Write(words[i] + ", ");
+                    }
+                    Console.WriteLine("HASHTAGS: ");
+                    for (int i = 0; i < hashtags.Count; i++)
+                    {
+                        Console.Write(words[i] + ", ");
+                    }
+                    Console.WriteLine("POLITICIAN: ");
+                    Console.WriteLine(politican.ToString());
+                    Console.ReadLine();
+                    words = new List<string>();
+                    politican = new StringBuilder();
+                    hashtags = new List<string>();
+                }
+                //Console.ReadLine();
+            }   
+            Console.ReadLine();
         }
-
     }
 }
