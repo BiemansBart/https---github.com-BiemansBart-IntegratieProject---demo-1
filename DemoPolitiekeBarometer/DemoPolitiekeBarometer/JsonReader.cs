173e299b4425c2a39d6920d65790653c73798aa3
Bart Biemans <bart.biemans@student.kdg.be>
2018-03-13 13:39:08 +0100

    extra edits jsonreader

--- DemoPolitiekeBarometer/DemoPolitiekeBarometer/JsonReader.cs
+++ DemoPolitiekeBarometer/DemoPolitiekeBarometer/JsonReader.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace DemoPolitiekeBarometer
 {
-    class Program
+    class JsonReader
     {
-        static void Main(string[] args)
-        {
-            
-        }
+
     }
 }
---
+++ DemoPolitiekeBarometer/DemoPolitiekeBarometer/Tweet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPolitiekeBarometer
+{
+    public class Tweet
+    {
+        public long TweetId { get; set; }
+        public string User_id { get; set; }
+        public string Geo { get; set; }
+        public List<string> Mentions { get; set; }
+        public bool Retweet { get; set; }
+        public DateTime Date { get; set; }
+        public List<string> Words { get; set; }
+        public int[] Sentiment { get; set; }
+        public Arr MyProperty { get; set; }
+    }
+}
